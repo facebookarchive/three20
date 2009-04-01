6785eedd330d9d07e241f2ea1d30ebc846b804d4
Joe Hewitt <joe@joehewitt.com>
2009-03-31 20:30:51 -0700

    * Use content inset in the styled label sample

--- samples/TTCatalog/Classes/StyledTextTestController.m
+++ samples/TTCatalog/Classes/StyledTextTestController.m
@@ -14,18 +14,18 @@
 <img src=\"bundle://smiley.png\"/>. You can also embed a URL inline and it will be turned into \
 a link, like the following URL: http://www.foo.com";
 
-  TTStyledLabel* label1 = [[[TTStyledLabel alloc] initWithFrame:
-                            CGRectInset(self.view.bounds, 10, 10)] autorelease];
+  TTStyledLabel* label1 = [[[TTStyledLabel alloc] initWithFrame:self.view.bounds] autorelease];
   label1.font = [UIFont systemFontOfSize:17];
   label1.text = [TTStyledText textFromXHTML:kText];
+  label1.contentInset = UIEdgeInsetsMake(10, 10, 10, 10);
   [label1 sizeToFit];
   [self.view addSubview:label1];
 
-  TTStyledLabel* label2 = [[[TTStyledLabel alloc] initWithFrame:
-                            CGRectInset(self.view.bounds, 10, 10)] autorelease];
+  TTStyledLabel* label2 = [[[TTStyledLabel alloc] initWithFrame:self.view.bounds] autorelease];
   label2.font = [UIFont systemFontOfSize:12];
   label2.text = [TTStyledText textFromXHTML:kText];
   label2.textColor = [UIColor grayColor];
+  label2.contentInset = UIEdgeInsetsMake(10, 10, 10, 10);
   [label2 sizeToFit];
   label2.top = label1.bottom + 20;
   [self.view addSubview:label2];
