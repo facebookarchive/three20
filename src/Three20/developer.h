2cc1201cbba8a556a5a723b20ae3122dee3ba4a2
Joe Hewitt <joe@joehewitt.com>
2009-04-10 00:28:16 -0700

    * Correct explanation of how to use developer.h

--- src/Three20/developer.h
+++ src/Three20/developer.h
@@ -1,5 +1,7 @@
 /*
  * This file contains developer-specific preprocessor definitions.  It exists mostly to prevent
- * Joe from continually checking in debugging code.  If you modify this file, be sure to add it
- * to the git exclude files so you don't check in your changes.
+ * Joe from continually checking in debugging code.  If you modify this file, run this command
+ * to tell git to ignore your changes:
+ *
+ *    git update-index --assume-unchanged src/Three20/developer.h
  */
