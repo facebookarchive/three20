0613635d82e2426ec54106dc5abc36724226980b
Owen Yamauchi <oyamauchi@facebook.com>
2009-12-02 16:33:11 -0800

    Version string comparison; see comment in NSStringAdditions.h for details

--- src/NSStringAdditions.m
+++ src/NSStringAdditions.m
@@ -149,4 +149,33 @@
   [[TTNavigator navigator] openURL:self query:query animated:YES];
 }
 
+- (NSComparisonResult)versionStringCompare:(NSString *)other {
+  NSArray *oneComponents = [self componentsSeparatedByString:@"a"];
+  NSArray *twoComponents = [other componentsSeparatedByString:@"a"];
+
+  // The parts before the "a"
+  NSString *oneMain = [oneComponents objectAtIndex:0];
+  NSString *twoMain = [twoComponents objectAtIndex:0];
+
+  // If main parts are different, return that result, regardless of alpha part
+  NSComparisonResult mainDiff;
+  if ((mainDiff = [oneMain compare:twoMain]) != NSOrderedSame) {
+    return mainDiff;
+  }
+
+  // At this point the main parts are the same; just deal with alpha stuff
+  // If one has an alpha part and the other doesn't, the one without is newer
+  if ([oneComponents count] < [twoComponents count]) {
+    return NSOrderedDescending;
+  } else if ([oneComponents count] > [twoComponents count]) {
+    return NSOrderedAscending;
+  }
+
+  // At this point the main parts are the same and both have alpha parts. Compare the alpha parts
+  // numerically. If it's not a valid number (including empty string) it's treated as zero.
+  NSNumber *oneAlpha = [NSNumber numberWithInt:[[oneComponents objectAtIndex:1] intValue]];
+  NSNumber *twoAlpha = [NSNumber numberWithInt:[[twoComponents objectAtIndex:1] intValue]];
+  return [oneAlpha compare:twoAlpha];
+}
+
 @end
--- src/Three20/NSStringAdditions.h
+++ src/Three20/NSStringAdditions.h
@@ -39,4 +39,36 @@
 - (void)openURL;
 - (void)openURLFromButton:(UIView*)button;
 
+/**
+ * Compares two strings expressing software versions.
+ *
+ * The comparison is (except for the development version provisions noted below) lexicographic
+ * string comparison. So as long as the strings being compared use consistent version formats,
+ * a variety of schemes are supported. For example "3.02" < "3.03" and "3.0.2" < "3.0.3". If you
+ * mix such schemes, like trying to compare "3.02" and "3.0.3", the result may not be what you
+ * expect.
+ *
+ * Development versions are also supported by adding an "a" character and more version info after
+ * it. For example "3.0a1" or "3.01a4". The way these are handled is as follows: if the parts
+ * before the "a" are different, the parts after the "a" are ignored. If the parts before the "a"
+ * are identical, the result of the comparison is the result of NUMERICALLY comparing the parts
+ * after the "a". If the part after the "a" is empty, it is treated as if it were "0". If one
+ * string has an "a" and the other does not (e.g. "3.0" and "3.0a1") the one without the "a"
+ * is newer.
+ *
+ * Examples (?? means undefined):
+ *   "3.0" > "2.5"
+ *   "3.1" > "3.0"
+ *   "3.0a1" < "3.0"
+ *   "3.0a1" < "3.0a4"
+ *   "3.0a2" < "3.0a19"  <-- careful
+ *   "3.0a" < "3.0a1"
+ *   "3.02" < "3.03"
+ *   "3.0.2" < "3.0.3"
+ *   "3.00" ?? "3.0"
+ *   "3.02" ?? "3.0.3"
+ *   "3.02" ?? "3.0.2"
+ */
+- (NSComparisonResult)versionStringCompare:(NSString *)other;
+
 @end
