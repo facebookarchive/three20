b4b2673dfc7b37352fffd637807d1a6544d61fdd
Jeff Verkoeyen <jverkoey@gmail.com>
2009-12-09 10:43:02 -0800

    Remove tabs.

--- src/UIColorAdditions.m
+++ src/UIColorAdditions.m
@@ -23,77 +23,77 @@
 #define MIN3(a,b,c) (a < b ? (a < c ? a : c) : (b < c ? b : c))
 
 void RGBtoHSV(float r, float g, float b, float* h, float* s, float* v) {
-	float min, max, delta;
-	min = MIN3(r, g, b);
-	max = MAX3(r, g, b);
-	*v = max;				// v
-	delta = max - min;
-	if( max != 0 )
-		*s = delta / max;		// s
-	else {
-		// r = g = b = 0		// s = 0, v is undefined
-		*s = 0;
-		*h = -1;
-		return;
-	}
-	if( r == max )
-		*h = ( g - b ) / delta;		// between yellow & magenta
-	else if( g == max )
-		*h = 2 + ( b - r ) / delta;	// between cyan & yellow
-	else
-		*h = 4 + ( r - g ) / delta;	// between magenta & cyan
-	*h *= 60;				// degrees
-	if( *h < 0 )
-		*h += 360;
+  float min, max, delta;
+  min = MIN3(r, g, b);
+  max = MAX3(r, g, b);
+  *v = max;        // v
+  delta = max - min;
+  if( max != 0 )
+    *s = delta / max;    // s
+  else {
+    // r = g = b = 0    // s = 0, v is undefined
+    *s = 0;
+    *h = -1;
+    return;
+  }
+  if( r == max )
+    *h = ( g - b ) / delta;    // between yellow & magenta
+  else if( g == max )
+    *h = 2 + ( b - r ) / delta;  // between cyan & yellow
+  else
+    *h = 4 + ( r - g ) / delta;  // between magenta & cyan
+  *h *= 60;        // degrees
+  if( *h < 0 )
+    *h += 360;
 }
 
 void HSVtoRGB( float *r, float *g, float *b, float h, float s, float v )
 {
-	int i;
-	float f, p, q, t;
-	if( s == 0 ) {
-		// achromatic (grey)
-		*r = *g = *b = v;
-		return;
-	}
-	h /= 60;			// sector 0 to 5
-	i = floor( h );
-	f = h - i;			// factorial part of h
-	p = v * ( 1 - s );
-	q = v * ( 1 - s * f );
-	t = v * ( 1 - s * ( 1 - f ) );
-	switch( i ) {
-		case 0:
-			*r = v;
-			*g = t;
-			*b = p;
-			break;
-		case 1:
-			*r = q;
-			*g = v;
-			*b = p;
-			break;
-		case 2:
-			*r = p;
-			*g = v;
-			*b = t;
-			break;
-		case 3:
-			*r = p;
-			*g = q;
-			*b = v;
-			break;
-		case 4:
-			*r = t;
-			*g = p;
-			*b = v;
-			break;
-		default:		// case 5:
-			*r = v;
-			*g = p;
-			*b = q;
-			break;
-	}
+  int i;
+  float f, p, q, t;
+  if( s == 0 ) {
+    // achromatic (grey)
+    *r = *g = *b = v;
+    return;
+  }
+  h /= 60;      // sector 0 to 5
+  i = floor( h );
+  f = h - i;      // factorial part of h
+  p = v * ( 1 - s );
+  q = v * ( 1 - s * f );
+  t = v * ( 1 - s * ( 1 - f ) );
+  switch( i ) {
+    case 0:
+      *r = v;
+      *g = t;
+      *b = p;
+      break;
+    case 1:
+      *r = q;
+      *g = v;
+      *b = p;
+      break;
+    case 2:
+      *r = p;
+      *g = v;
+      *b = t;
+      break;
+    case 3:
+      *r = p;
+      *g = q;
+      *b = v;
+      break;
+    case 4:
+      *r = t;
+      *g = p;
+      *b = v;
+      break;
+    default:    // case 5:
+      *r = v;
+      *g = p;
+      *b = q;
+      break;
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
