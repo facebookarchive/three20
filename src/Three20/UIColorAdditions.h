4a18d141d9f95986a289d006a96ba63d3cd78491
Jeff Verkoeyen <jverkoey@gmail.com>
2009-12-09 11:20:36 -0800

    Document UIColorAdditions.

--- src/Three20/UIColorAdditions.h
+++ src/Three20/UIColorAdditions.h
@@ -19,21 +19,37 @@
 
 @interface UIColor (TTCategory)
 
+/**
+ * Accepted ranges:
+ *        hue: 0.0 - 360.0
+ * saturation: 0.0 - 1.0
+ *      value: 0.0 - 1.0
+ *      alpha: 0.0 - 1.0
+ */
 + (UIColor*)colorWithHue:(CGFloat)h saturation:(CGFloat)s value:(CGFloat)v alpha:(CGFloat)a;
 
+/**
+ * Accepted ranges:
+ *        hue: 0.0 - 1.0
+ * saturation: 0.0 - 1.0
+ *      value: 0.0 - 1.0
+ */
 - (UIColor*)multiplyHue:(CGFloat)hd saturation:(CGFloat)sd value:(CGFloat)vd;
 
 - (UIColor*)addHue:(CGFloat)hd saturation:(CGFloat)sd value:(CGFloat)vd;
 
+/**
+ * Returns a new UIColor with the given alpha.
+ */
 - (UIColor*)copyWithAlpha:(CGFloat)newAlpha;
 
 /**
- * Uses multiplyHue to create a lighter version of the color.
+ * Uses multiplyHue:saturation:value:alpha: to create a lighter version of the color.
  */
 - (UIColor*)highlight;
 
 /**
- * Uses multiplyHue to create a darker version of the color.
+ * Uses multiplyHue:saturation:value:alpha: to create a darker version of the color.
  */
 - (UIColor*)shadow;
 
