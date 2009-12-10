aac67179a61ca2db78a81fa94fa6b0a1c96c60be
Jeff Verkoeyen <jverkoey@gmail.com>
2009-12-10 03:34:19 -0800

    [Minor] Fix the license header to use // instead of /** for doxygen.

--- src/tests/UIAdditionTests.m
+++ src/tests/UIAdditionTests.m
@@ -1,18 +1,18 @@
-/**
- * Copyright 2009 Facebook
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+// Copyright 2009 Facebook
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 // See: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/905-A-Unit-Test_Result_Macro_Reference/unit-test_results.html#//apple_ref/doc/uid/TP40007959-CH21-SW2
 // for unit test macros.
@@ -40,8 +40,6 @@
 - (BOOL)color:(UIColor*)color1 equalsColor:(UIColor*)color2 {
   const CGFloat* rgba1 = CGColorGetComponents(color1.CGColor);
   const CGFloat* rgba2 = CGColorGetComponents(color2.CGColor);
-  NSLog(@"hai: %f %f %f %f", rgba1[0], rgba1[1], rgba1[2], rgba1[3]);
-  NSLog(@"ha2: %f %f %f %f", rgba2[0], rgba2[1], rgba2[2], rgba2[3]);
   return 0 == memcmp(rgba1, rgba2, sizeof(CGFloat) * 4);
 }
 
