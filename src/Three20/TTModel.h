b8ef2c0ce975649e271b59ef09af55234bd16771
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-06 04:01:19 -0400

    [Dox] Clean up TTModel.

--- src/Three20/TTModel.h
+++ src/Three20/TTModel.h
@@ -31,36 +31,50 @@
 
 /**
  * Indicates that the data has been loaded.
+ *
+ * Default implementation returns YES.
  */
 - (BOOL)isLoaded;
 
 /**
  * Indicates that the data is in the process of loading.
+ *
+ * Default implementation returns NO.
  */
 - (BOOL)isLoading;
 
 /**
  * Indicates that the data is in the process of loading additional data.
+ *
+ * Default implementation returns NO.
  */
 - (BOOL)isLoadingMore;
 
 /**
  * Indicates that the model is of date and should be reloaded as soon as possible.
+ *
+ * Default implementation returns NO.
  */
 -(BOOL)isOutdated;
 
 /**
  * Loads the model.
+ *
+ * Default implementation does nothing.
  */
 - (void)load:(TTURLRequestCachePolicy)cachePolicy more:(BOOL)more;
 
 /**
  * Cancels a load that is in progress.
+ *
+ * Default implementation does nothing.
  */
 - (void)cancel;
 
 /**
  * Invalidates data stored in the cache or optionally erases it.
+ *
+ * Default implementation does nothing.
  */
 - (void)invalidate:(BOOL)erase;
 
