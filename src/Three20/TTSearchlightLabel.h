fa855f24671f4d9b93d95227ec3acc6b0834091a
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-09 20:21:03 -0400

    [Dox] Clean up TTSearchlightLabel.

--- src/Three20/TTSearchlightLabel.h
+++ src/Three20/TTSearchlightLabel.h
@@ -18,22 +18,26 @@
 #import <UIKit/UIKit.h>
 
 @interface TTSearchlightLabel : UIView {
-  NSString* _text;
-  UIFont* _font;
-  UIColor* textColor;
-  UIColor* _spotlightColor;
+  NSString*       _text;
+
+  UIFont*         _font;
+  UIColor*        _textColor;
+  UIColor*        _spotlightColor;
   UITextAlignment _textAlignment;
-  NSTimer* _timer;
-  CGFloat _spotlightPoint;
-  CGContextRef _maskContext;
-  void* _maskData;
+
+  NSTimer*        _timer;
+  CGFloat         _spotlightPoint;
+
+  CGContextRef    _maskContext;
+  void*           _maskData;
 }
 
-@property (nonatomic, copy) NSString* text;
-@property (nonatomic, retain) UIFont* font;
-@property (nonatomic, retain) UIColor* textColor;
-@property (nonatomic, retain) UIColor* spotlightColor;
-@property (nonatomic) UITextAlignment textAlignment;
+@property (nonatomic, copy)   NSString*       text;
+
+@property (nonatomic, retain) UIFont*         font;
+@property (nonatomic, retain) UIColor*        textColor;
+@property (nonatomic, retain) UIColor*        spotlightColor;
+@property (nonatomic)         UITextAlignment textAlignment;
 
 - (void)startAnimating;
 - (void)stopAnimating;
