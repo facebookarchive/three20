86a74c8159a747f91f0080d794e3c6949eb593a2
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-09 20:03:22 -0400

    [Dox] Clean up TTLabel.

--- src/Three20/TTLabel.h
+++ src/Three20/TTLabel.h
@@ -18,11 +18,11 @@
 
 @interface TTLabel : TTView {
   NSString* _text;
-  UIFont* _font;
+  UIFont*   _font;
 }
 
-@property (nonatomic, copy) NSString* text;
-@property (nonatomic, retain) UIFont* font;
+@property (nonatomic, copy)   NSString* text;
+@property (nonatomic, retain) UIFont*   font;
 
 - (id)initWithText:(NSString*)text;
 
