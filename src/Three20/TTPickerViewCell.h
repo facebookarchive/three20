2bb926f63012625a1879e532b1e528e24b0d2b9d
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-13 18:10:42 -0400

    [Dox] Clean up TTPickerViewCell.

--- src/Three20/TTPickerViewCell.h
+++ src/Three20/TTPickerViewCell.h
@@ -17,14 +17,14 @@
 #import "Three20/TTView.h"
 
 @interface TTPickerViewCell : TTView {
-  id _object;
-  UILabel* _labelView;
-  BOOL _selected;
+  id        _object;
+  UILabel*  _labelView;
+  BOOL      _selected;
 }
 
-@property (nonatomic, retain) id object;
-@property (nonatomic, copy) NSString* label;
-@property (nonatomic, retain) UIFont* font;
-@property (nonatomic) BOOL selected;
+@property (nonatomic, retain) id        object;
+@property (nonatomic, copy)   NSString* label;
+@property (nonatomic, retain) UIFont*   font;
+@property (nonatomic)         BOOL      selected;
 
 @end
