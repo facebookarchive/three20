64abae29204874f9b730a92621049cfeb97fcd3d
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-13 18:45:45 -0400

    [Dox] Clean up TTErrorView.

--- src/Three20/TTErrorView.h
+++ src/Three20/TTErrorView.h
@@ -18,14 +18,14 @@
 #import <UIKit/UIKit.h>
 
 @interface TTErrorView : UIView {
-  UIImageView* _imageView;
-  UILabel* _titleView;
-  UILabel* _subtitleView;
+  UIImageView*  _imageView;
+  UILabel*      _titleView;
+  UILabel*      _subtitleView;
 }
 
-@property (nonatomic, retain) UIImage* image;
-@property (nonatomic, copy) NSString* title;
-@property (nonatomic, copy) NSString* subtitle;
+@property (nonatomic, retain) UIImage*  image;
+@property (nonatomic, copy)   NSString* title;
+@property (nonatomic, copy)   NSString* subtitle;
 
 - (id)initWithTitle:(NSString*)title subtitle:(NSString*)subtitle image:(UIImage*)image;
 
