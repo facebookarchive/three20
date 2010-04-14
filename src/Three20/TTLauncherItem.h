03143b8df95d8a37da7431f132c1224d1f9dc3fa
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-14 17:58:22 -0400

    [Dox] Clean up TTLauncherItem.

--- src/Three20/TTLauncherItem.h
+++ src/Three20/TTLauncherItem.h
@@ -21,21 +21,24 @@
 
 @interface TTLauncherItem : NSObject <NSCoding> {
   TTLauncherView* _launcher;
-  NSString* _title;
-  NSString* _image;
-  NSString* _URL;
-  NSString* _style;
-  NSInteger _badgeNumber;
-  BOOL _canDelete;
+
+  NSString*       _title;
+  NSString*       _image;
+  NSString*       _URL;
+
+  NSString*       _style;
+  NSInteger       _badgeNumber;
+
+  BOOL            _canDelete;
 }
 
 @property (nonatomic, assign) TTLauncherView* launcher;
-@property (nonatomic, copy) NSString* title;
-@property (nonatomic, copy) NSString* image;
-@property (nonatomic, copy) NSString* URL;
-@property (nonatomic, copy) NSString* style;
-@property (nonatomic) NSInteger badgeNumber;
-@property (nonatomic) BOOL canDelete;
+@property (nonatomic, copy) 	NSString*       title;
+@property (nonatomic, copy)   NSString*       image;
+@property (nonatomic, copy) 	NSString*       URL;
+@property (nonatomic, copy) 	NSString*       style;
+@property (nonatomic)         NSInteger       badgeNumber;
+@property (nonatomic)         BOOL            canDelete;
 
 - (id)initWithTitle:(NSString*)title image:(NSString*)image URL:(NSString*)URL;
 - (id)initWithTitle:(NSString*)title image:(NSString*)image URL:(NSString*)URL
