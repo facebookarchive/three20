50117728cd8f291e8e100877e689093d9fa671f8
Jeff Verkoeyen <jverkoey@gmail.com>
2010-04-24 09:05:34 -0700

    Remove \mainpage tag in the JSON documentation.
    This was causing the JSON documentation to override the Three20
    documentation when generating dox.

--- src/JSON/JSON.h
+++ src/JSON/JSON.h
@@ -1,20 +1,20 @@
 /*
  Copyright (C) 2009 Stig Brautaset. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  * Redistributions of source code must retain the above copyright notice, this
    list of conditions and the following disclaimer.
- 
+
  * Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
- 
+
  * Neither the name of the author nor the names of its contributors may be used
    to endorse or promote products derived from this software without specific
    prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,7 +28,7 @@
  */
 
 /**
- @mainpage A strict JSON parser and generator for Objective-C
+ A strict JSON parser and generator for Objective-C
 
  JSON (JavaScript Object Notation) is a lightweight data-interchange
  format. This framework provides two apis for parsing and generating
@@ -36,12 +36,12 @@
  categories added to existing Objective-C classes.
 
  Learn more on the http://code.google.com/p/json-framework project site.
- 
+
  This framework does its best to be as strict as possible, both in what it
  accepts and what it generates. For example, it does not support trailing commas
  in arrays or objects. Nor does it support embedded comments, or
- anything else not in the JSON specification. This is considered a feature. 
- 
+ anything else not in the JSON specification. This is considered a feature.
+
 */
 
 #import "SBJSON.h"
