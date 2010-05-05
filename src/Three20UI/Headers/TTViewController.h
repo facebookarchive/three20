3bbacd9894042d911e5ca376c71c1586ca3cc207
Jeff Verkoeyen <jverkoey@gmail.com>
2010-05-05 19:35:32 -0400

    [UICommon|UI] Create a TTBaseViewController in UICommon.
    This will allow us to check for types of TTBaseViewControllers in UICommon
    and UINavigator. TTViewController inherits from TTBaseViewController.

--- src/Three20UI/Headers/TTViewController.h
+++ src/Three20UI/Headers/TTViewController.h
@@ -14,8 +14,7 @@
 // limitations under the License.
 //
 
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+#import "Three20UICommon/TTBaseViewController.h"
 
 @class TTTableViewController;
 @class TTSearchDisplayController;
@@ -23,42 +22,10 @@
 /**
  * A view controller with some useful additions.
  */
-@interface TTViewController : UIViewController {
+@interface TTViewController : TTBaseViewController {
 @protected
-  NSDictionary*     _frozenState;
-  UIBarStyle        _navigationBarStyle;
-  UIColor*          _navigationBarTintColor;
-  UIStatusBarStyle  _statusBarStyle;
-
   TTSearchDisplayController* _searchController;
-
-  BOOL _isViewAppearing;
-  BOOL _hasViewAppeared;
-  BOOL _autoresizesForKeyboard;
 }
-
-/**
- * The style of the navigation bar when this view controller is pushed onto
- * a navigation controller.
- *
- * @default UIBarStyleDefault
- */
-@property (nonatomic) UIBarStyle navigationBarStyle;
-
-/**
- * The color of the navigation bar when this view controller is pushed onto
- * a navigation controller.
- *
- * @default TTSTYLEVAR(navigationBarTintColor)
- */
-@property (nonatomic, retain) UIColor* navigationBarTintColor;
-
-/**
- * The style of the status bar when this view controller is appearing.
- *
- * @default UIStatusBarStyleDefault
- */
-@property (nonatomic) UIStatusBarStyle statusBarStyle;
 
 /**
  * A view controller used to display the contents of the search display controller.
@@ -69,42 +36,6 @@
  * search bar will then search the data source of the view controller that you assigned here.
  */
 @property (nonatomic, retain) TTTableViewController* searchViewController;
-
-/**
- * The view has appeared at least once and hasn't been removed due to a memory warning.
- */
-@property (nonatomic, readonly) BOOL hasViewAppeared;
-
-/**
- * The view is about to appear and has not appeared yet.
- */
-@property (nonatomic, readonly) BOOL isViewAppearing;
-
-/**
- * Determines if the view will be resized automatically to fit the keyboard.
- */
-@property (nonatomic) BOOL autoresizesForKeyboard;
-
-
-/**
- * Sent to the controller before the keyboard slides in.
- */
-- (void)keyboardWillAppear:(BOOL)animated withBounds:(CGRect)bounds;
-
-/**
- * Sent to the controller before the keyboard slides out.
- */
-- (void)keyboardWillDisappear:(BOOL)animated withBounds:(CGRect)bounds;
-
-/**
- * Sent to the controller after the keyboard has slid in.
- */
-- (void)keyboardDidAppear:(BOOL)animated withBounds:(CGRect)bounds;
-
-/**
- * Sent to the controller after the keyboard has slid out.
- */
-- (void)keyboardDidDisappear:(BOOL)animated withBounds:(CGRect)bounds;
 
 
 @end
