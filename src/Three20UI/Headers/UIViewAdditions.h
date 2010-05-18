3eb99253f998b88bc4cda5cc8d5ef0d0def7ba95
Chris Donnelly <cd+github@69bytes.com>
2010-05-18 14:26:52 -0700

    Added a "popupTargetView" member to UIView that returns the superview in which a UIActionSheet or TTActionSheetController should be told to show itself.  This is because using -[UIView window] does not properly handle interface rotation.

--- src/Three20UI/Headers/UIViewAdditions.h
+++ src/Three20UI/Headers/UIViewAdditions.h
@@ -177,4 +177,15 @@
  */
 - (UIViewController*)viewController;
 
+/**
+ * The view (ancestor or self) in which to open popup views (e.g. UIAlertView, UIActionSheet).
+ *
+ * This is to avoid problems like those found here, but in a way that respects rotation:
+ *  http://stackoverflow.com/questions/1197746/uiactionsheet-cancel-button-strange-behaviour
+ *  http://nerdhappy.blogspot.com/2009/12/uiactionsheet-vs-uitabbarcontroller.html
+ *
+ * On rotated screens, the window is NOT rotated, and so we cannot use -[UIView window] as the desired target.
+ */
+- (UIView*)popupTargetView;
+
 @end
