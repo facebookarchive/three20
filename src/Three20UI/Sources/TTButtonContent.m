2e6c07fb8e28329cdb031aec2c4650255dec1866
Jeff Verkoeyen <jverkoey@gmail.com>
2010-07-07 10:43:08 -0400

    [UI] Also call didLoadImage: when the image is loaded from cache.

--- src/Three20UI/Sources/TTButtonContent.m
+++ src/Three20UI/Sources/TTButtonContent.m
@@ -142,6 +142,10 @@
     if (image) {
       self.image = image;
       [_button setNeedsDisplay];
+
+      if ([_delegate respondsToSelector:@selector(imageView:didLoadImage:)]) {
+        [_delegate imageView:nil didLoadImage:image];
+      }
     } else {
       TTURLRequest* request = [TTURLRequest requestWithURL:_imageURL delegate:self];
       request.response = [[[TTURLImageResponse alloc] init] autorelease];
