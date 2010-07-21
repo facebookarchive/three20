e4fa8ab1f3cef485ec65a7f54e35682057e4d7d8
Paulo Oliveira <Paulo@112-235-191-190.cab.prima.net.ar>
2010-07-21 17:08:14 -0300

    TTScrollView Improvements:
    With this improvement we're almost identical to the iPhone Photo Album functionalities:
    
    * Add zoom in/out based on the velocity that the fingers move on the pinch gesture.
    * Add zoom in/out from or to the center of fingers on every zoom level.
    * Add residual movement with deceleration after the user lifts their finger when dragging zoomed images.
    
    New zoom engine also include a few convenient properties and public methods:
    
    * Allows define the rate of deceleration after the user lifts their finger. (property).
    
    * Allows to set the maximum scale factor that can be applied to the scroll view's content. (property).
    
    * Allows to set the current zoom scale. Optionally animated, and optionally specifying the anchor point (methods).

--- src/Three20UI/Headers/TTScrollView.h
+++ src/Three20UI/Headers/TTScrollView.h
@@ -21,50 +21,70 @@
 @protocol TTScrollViewDataSource;
 
 @interface TTScrollView : UIView {
-  NSInteger       _centerPageIndex;
-  NSInteger       _visiblePageIndex;
-  BOOL            _scrollEnabled;
-  BOOL            _zoomEnabled;
-  BOOL            _rotateEnabled;
-  CGFloat         _pageSpacing;
-  NSTimeInterval  _holdsAfterTouchingForInterval;
-
-  UIInterfaceOrientation  _orientation;
-
-  id<TTScrollViewDelegate>    _delegate;
-  id<TTScrollViewDataSource>  _dataSource;
-
-  NSMutableArray* _pages;
-  NSMutableArray* _pageQueue;
-  NSInteger       _maxPages;
-  NSInteger       _pageArrayIndex;
-  NSTimer*        _tapTimer;
-  NSTimer*        _holdingTimer;
-  NSTimer*        _animationTimer;
-  NSDate*         _animationStartTime;
-  NSTimeInterval  _animationDuration;
-  UIEdgeInsets    _animateEdges;
-
-  // The offset of the page edges from the edge of the screen.
-  UIEdgeInsets    _pageEdges;
-
-  // At the beginning of an animation, the page edges are cached within this member.
-  UIEdgeInsets    _pageStartEdges;
-
-  UIEdgeInsets    _touchEdges;
-  UIEdgeInsets    _touchStartEdges;
-  NSUInteger      _touchCount;
-  CGFloat         _overshoot;
-
-  // The first touch in this view.
-  UITouch*        _touch1;
-
-  // The second touch in this view.
-  UITouch*        _touch2;
-
-  BOOL            _dragging;
-  BOOL            _zooming;
-  BOOL            _holding;
+	NSInteger       _centerPageIndex;
+	NSInteger       _visiblePageIndex;
+	BOOL            _scrollEnabled;
+	BOOL            _zoomEnabled;
+	BOOL            _rotateEnabled;
+	CGFloat         _pageSpacing;
+	NSTimeInterval  _holdsAfterTouchingForInterval;
+	
+	UIInterfaceOrientation  _orientation;
+	
+	id<TTScrollViewDelegate>    _delegate;
+	id<TTScrollViewDataSource>  _dataSource;
+	
+	NSMutableArray* _pages;
+	NSMutableArray* _pageQueue;
+	NSInteger       _maxPages;
+	NSInteger       _pageArrayIndex;
+	NSTimer*        _tapTimer;
+	NSTimer*        _holdingTimer;
+	NSTimer*        _animationTimer;
+	NSDate*         _animationStartTime;
+	NSTimeInterval  _animationDuration;
+	UIEdgeInsets    _animateEdges;
+	
+	// Speed for Inertia.
+	CGPoint		  _inertiaSpeed;
+	CGPoint		  _renewPosition;
+	
+	// A floating-point value that specifies the maximum scale factor that can be applied to the scroll view's content.
+	CGFloat _maximumZoomScale;
+	CGFloat _zoomScale;
+	
+	// Middle point bewteen fingers, is used to zoom using fingers positon and not the center of the image.
+	CGPoint centerOfFingers;
+	
+	// Distance between fingers, used to calculate zoom scale and zoom speed rate.
+	CGFloat actualDistanceBetweenFingers;
+	CGFloat distanceBetweenFingers;
+	
+	// A floating-point value that determines the rate of deceleration after the user lifts their finger.
+	CGFloat _decelerationRate;
+	
+	// The offset of the page edges from the edge of the screen.
+	UIEdgeInsets    _pageEdges;
+	
+	// At the beginning of an animation, the page edges are cached within this member.
+	UIEdgeInsets    _pageStartEdges;
+	
+	UIEdgeInsets    _touchEdges;
+	UIEdgeInsets    _touchStartEdges;
+	NSUInteger      _touchCount;
+	CGFloat         _overshoot;
+	
+	// The first touch in this view.
+	UITouch*        _touch1;
+	
+	// The second touch in this view.
+	UITouch*        _touch2;
+	
+	BOOL            _dragging;
+	BOOL			_decelerating;
+	BOOL            _zooming;
+	BOOL			_executingZoomGesture;
+	BOOL            _holding;
 }
 
 /**
@@ -77,7 +97,20 @@
  */
 @property (nonatomic, readonly) BOOL zoomed;
 
+/**
+ * A Boolean value that indicates whether the content view is currently zooming in or out. (read-only)
+ *
+ * The value of this property is YES if user is making a zoom gesture, otherwise it is NO 
+ *
+ */
+@property (nonatomic, readonly) BOOL zooming;
+
 @property (nonatomic, readonly) BOOL holding;
+
+/**
+ * Returns whether the content is moving in the scroll view after the user lifted their finger. (read-only)
+ */
+@property(nonatomic,readonly,getter=isDecelerating) BOOL decelerating;   
 
 /**
  * @default YES
@@ -110,7 +143,32 @@
  */
 @property (nonatomic) NSTimeInterval holdsAfterTouchingForInterval;
 
+/**
+ * A floating-point value that determines the rate of deceleration after the user lifts their finger.
+ *
+ * @default 0.9
+ */
+@property CGFloat decelerationRate;
 
+/**
+ * A floating-point value that specifies the current scale factor applied to the scroll view's content.
+ *
+ * The scale is animated by Default, use setZoomScale:animated: to control when is animated or not.
+ *
+ * @default 1.0
+ */
+@property(nonatomic,assign) CGFloat zoomScale;
+
+/** 
+ * A floating-point value that specifies the maximum scale factor that 
+ * can be applied to the scroll view's content.
+ *
+ * @default 4.0
+ */
+@property(nonatomic) CGFloat maximumZoomScale;
+
+
+/// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// /// 
 @property (nonatomic, assign) id<TTScrollViewDelegate>    delegate;
 @property (nonatomic, assign) id<TTScrollViewDataSource>  dataSource;
 
@@ -120,6 +178,23 @@
 @property (nonatomic, readonly) NSDictionary* visiblePages;
 
 - (void)setOrientation:(UIInterfaceOrientation)orientation animated:(BOOL)animated;
+
+/**
+ * A floating-point value that specifies the current zoom scale.
+ * 
+ * YES to animate the transition to the new scale, NO to make the transition immediate.
+ */
+- (void)setZoomScale:(CGFloat)newScale animated:(BOOL)animated;
+
+/**
+ * A floating-point value that specifies the current zoom scale.
+ * 
+ * Specify one point to scale centering to him.
+ *
+ * YES to animate the transition to the new scale, NO to make the transition immediate.
+ */
+- (void)setZoomScale:(CGFloat)newScale withPoint:(CGPoint)withPoint animated:(BOOL)animated;
+
 
 /**
  * Gets a previously created page view that has been moved off screen and recycled.
