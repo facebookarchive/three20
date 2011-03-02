1a56c505a2b95773ce7017921d7371ec52ada691
Peter Steinberger <me@petersteinberger.com>
2011-03-02 14:19:50 +0100

    fixes a warning in TTStyledLayout

--- src/Three20Style/Sources/TTStyledLayout.m
+++ src/Three20Style/Sources/TTStyledLayout.m
@@ -758,7 +758,6 @@
                       sizeWithFont:_font].width;
         [self addFrameForText:line element:element node:textNode width:frameWidth
               height:[_font ttLineHeight]];
-        frameWidth = 0;
       }
     }
   }
