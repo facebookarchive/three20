c93086f10aa93949a03d3b097304128bfbe3919c
jverkoey <jverkoey@gmail.com>
2011-04-11 23:55:05 -0400

    [Core] Add availability headers for SDK availability testing.

--- src/Three20Core/Sources/TTAvailability.m
+++ src/Three20Core/Sources/TTAvailability.m
@@ -14,27 +14,20 @@
 // limitations under the License.
 //
 
-// Core
-
-// - Global
-#import "Three20Core/TTCorePreprocessorMacros.h"
-
-#import "Three20Core/TTGlobalCore.h"
-#import "Three20Core/TTGlobalCoreLocale.h"
-#import "Three20Core/TTGlobalCorePaths.h"
-#import "Three20Core/TTGlobalCoreRects.h"
 #import "Three20Core/TTAvailability.h"
 
-// - Debug
-#import "Three20Core/TTDebug.h"
 
-// - Extensions
-#import "Three20Core/TTExtensionInfo.h"
-#import "Three20Core/TTExtensionAuthor.h"
-#import "Three20Core/TTExtensionLoader.h"
+///////////////////////////////////////////////////////////////////////////////////////////////////
+BOOL TTDeviceOSVersionIsAtLeast(double versionNumber) {
+  return kCFCoreFoundationVersionNumber >= versionNumber;
+}
 
-// - Entity Tables
-#import "Three20Core/TTEntityTables.h"
 
-// - Classes
-#import "Three20Core/TTMarkupStripper.h"
+///////////////////////////////////////////////////////////////////////////////////////////////////
+Class TTUIPopoverControllerClass() {
+  static Class sClass = nil;
+  if (nil == sClass) {
+    sClass = NSClassFromString(@"UIPopoverController");
+  }
+  return sClass;
+}
