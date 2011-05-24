87eb13f4f361eec06b5c4a362b95803db90f0f45
Mike Morearty <mike@morearty.com>
2011-05-24 15:18:57 -0700

    Restore loading view, error view, empty view, and table banner if view is unloaded and then reloaded

--- src/Three20UI/Sources/TTModelViewController.m
+++ src/Three20UI/Sources/TTModelViewController.m
@@ -208,6 +208,7 @@
 - (void)didReceiveMemoryWarning {
   if (_hasViewAppeared && !_isViewAppearing) {
     [super didReceiveMemoryWarning];
+    [self resetViewStates];
     [self refresh];
 
   } else {
