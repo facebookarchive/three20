b6f5f08cdf3a31c9ed273c8aba46eac1140efded
aporat <adar.porat@me.com>
2011-07-11 13:22:57 -0400

    [extJSON] updated JSON library to version 3.0

--- src/extThree20JSON/Vendors/JSON/JSON.h
+++ src/extThree20JSON/Vendors/JSON/JSON.h
@@ -1,20 +1,20 @@
 /*
  Copyright (C) 2009-2010 Stig Brautaset. All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
-
+ 
  * Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
+ list of conditions and the following disclaimer.
+ 
  * Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
  * Neither the name of the author nor the names of its contributors may be used
-   to endorse or promote products derived from this software without specific
-   prior written permission.
-
+ to endorse or promote products derived from this software without specific
+ prior written permission.
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,38 +28,42 @@
  */
 
 /**
- A strict JSON parser and generator for Objective-C
-
+ @mainpage A strict JSON parser and generator for Objective-C
+ 
  JSON (JavaScript Object Notation) is a lightweight data-interchange
  format. This framework provides two apis for parsing and generating
  JSON. One standard object-based and a higher level api consisting of
  categories added to existing Objective-C classes.
-
- This framework does its best to be as strict as possible, both in what it accepts and what it generates. For example, it does not support trailing commas in arrays or objects. Nor does it support embedded comments, or anything else not in the JSON specification. This is considered a feature.
-
+ 
+ This framework does its best to be as strict as possible, both in what it accepts and what it generates. For example, it does not support trailing commas in arrays or objects. Nor does it support embedded comments, or anything else not in the JSON specification. This is considered a feature. 
+ 
  @section Links
-
+ 
  @li <a href="http://stig.github.com/json-framework">Project home page</a>.
- @li Online version of the <a href="http://stig.github.com/json-framework/api">API documentation</a>.
+ @li Online version of the <a href="http://stig.github.com/json-framework/api">API documentation</a>. 
+ 
+ */
 
-*/
 
-
-// This setting of 1 is best if you copy the source into your project.
+// This setting of 1 is best if you copy the source into your project. 
 // The build transforms the 1 to a 0 when building the framework and static lib.
 
 #if 1
 
 #import "extThree20JSON/SBJsonParser.h"
 #import "extThree20JSON/SBJsonWriter.h"
-#import "extThree20JSON/NSObject+SBJSON.h"
-#import "extThree20JSON/NSString+SBJSON.h"
+#import "extThree20JSON/SBJsonStreamWriter.h"
+#import "extThree20JSON/SBJsonStreamParser.h"
+#import "extThree20JSON/SBJsonStreamParserAdapter.h"
+#import "extThree20JSON/NSObject+JSON.h"
 
 #else
 
 #import <JSON/SBJsonParser.h>
 #import <JSON/SBJsonWriter.h>
-#import <JSON/NSObject+SBJSON.h>
-#import <JSON/NSString+SBJSON.h>
+#import <JSON/SBJsonStreamParser.h>
+#import <JSON/SBJsonStreamParserAdapter.h>
+#import <JSON/SBJsonStreamWriter.h>
+#import <JSON/NSObject+JSON.h>
 
 #endif
