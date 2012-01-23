2a03735e19b67028d42cabe909c56f5e7ba08e84
Sasmito Adibowo <adib@basil-salad.com>
2012-01-23 19:02:05 +0800

    Changed property access notation to method calls to silence out "Property access result unused - getters should not be used for side effects" warnings.

--- src/Three20UI/Sources/TTMessageController.m
+++ src/Three20UI/Sources/TTMessageController.m
@@ -423,7 +423,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)restoreView:(NSDictionary*)state {
-  self.view;
+  [self view];
   TT_RELEASE_SAFELY(_initialRecipients);
   NSMutableArray* fields = [state objectForKey:@"fields"];
   for (NSInteger i = 0; i < fields.count; ++i) {
@@ -540,7 +540,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSString*)subject {
-  self.view;
+  [self view];
   for (int i = 0; i < _fields.count; ++i) {
     id field = [_fields objectAtIndex:i];
     if ([field isKindOfClass:[TTMessageSubjectField class]]) {
@@ -554,7 +554,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)setSubject:(NSString*)subject {
-  self.view;
+  [self view];
   for (int i = 0; i < _fields.count; ++i) {
     id field = [_fields objectAtIndex:i];
     if ([field isKindOfClass:[TTMessageSubjectField class]]) {
@@ -574,7 +574,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)setBody:(NSString*)body {
-  self.view;
+  [self view];
   _textEditor.text = body;
 }
 
@@ -610,7 +610,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)addRecipient:(id)recipient forFieldAtIndex:(NSUInteger)fieldIndex {
-  self.view;
+  [self view];
   TTPickerTextField* textField = [_fieldViews objectAtIndex:fieldIndex];
   if ([textField isKindOfClass:[TTPickerTextField class]]) {
     NSString* label = [_dataSource tableView:textField.tableView labelForObject:recipient];
@@ -623,7 +623,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSString*)textForFieldAtIndex:(NSUInteger)fieldIndex {
-  self.view;
+  [self view];
 
   NSString* text = nil;
   if (fieldIndex == _fieldViews.count) {
@@ -643,7 +643,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (void)setText:(NSString*)text forFieldAtIndex:(NSUInteger)fieldIndex {
-  self.view;
+  [self view];
   if (fieldIndex == _fieldViews.count) {
     _textEditor.text = text;
 
@@ -658,7 +658,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (BOOL)fieldHasValueAtIndex:(NSUInteger)fieldIndex {
-  self.view;
+  [self view];
 
   if (fieldIndex == _fieldViews.count) {
     return _textEditor.text.length > 0;
@@ -682,7 +682,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (UIView*)viewForFieldAtIndex:(NSUInteger)fieldIndex {
-  self.view;
+  [self view];
 
   if (fieldIndex == _fieldViews.count) {
     return _textEditor;
