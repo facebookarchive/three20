9bd7e8c364d2d77f3ea1ad5a14d9686f8cad9aa2
Wú Dí <wudiac@gmail.com>
2012-11-23 00:12:26 +0900

    Update src/Three20UINavigator/Sources/TTBaseNavigator.m
    This cause a strange error (app goes to a black view after choose or cancel the image picker) when launch the image picker in TTWebView..

--- src/Three20UINavigator/Sources/TTBaseNavigator.m
+++ src/Three20UINavigator/Sources/TTBaseNavigator.m
@@ -250,7 +250,7 @@ __attribute__((weak_import));
       [_rootContainer navigator:self setRootViewController:_rootViewController];
 
     } else {
-      [self.window addSubview:_rootViewController.view];
+      [self.window setRootViewController:_rootViewController];
     }
   }
 }
